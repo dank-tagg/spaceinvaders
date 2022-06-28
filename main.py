7694a72affdc97d8070e3e3801efdf8f346e4ddf
dank-tagg <mianzhihu@gmail.com>
2022-06-28 12:54:51 +0200

    Game main loop and UI

--- main.py
+++ main.py
@@ -151,4 +151,122 @@ def collide(obj1, obj2):
     offset_y = obj2.y - obj1.y
     return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
+def main():
+    run = True
+    FPS = 60
+    level = 0
+    lives = 5
+    main_font = pygame.font.SysFont('comicsans', 50)
+    lost_font = pygame.font.SysFont('comicsans', 60)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+
+    player_vel = 5
+    laser_vel = 5
+
+    player = Player(300, 630)
+
+    clock = pygame.time.Clock()
+
+    lost = False
+    lost_count = 0
+
+    def redraw_window():
+        window.blit(background, (0,0))
+        # draw text
+        level_label = main_font.render(f'Level: {level}', 1, (255,255,255))
+        score_label = main_font.render(f'Score: {player.score}', 1, (255,255,255))
+
+        for n in range(lives):
+            window.blit(pygame.transform.scale(pygame.image.load('./assets/heart.png'), (125, 50)), (10+n*25, 0))
+        window.blit(level_label, (width - level_label.get_width() - 10, 10))
+        window.blit(score_label, (width - level_label.get_width() - 250, 10))
+
+        for enemy in enemies:
+            enemy.draw(window)
+
+        player.draw(window)
+
+        if lost:
+            lost_label = lost_font.render('You Lost!!', 1, (255,255,255))
+            window.blit(lost_label, (width/2 - lost_label.get_width()/2, 350))
+
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        if lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for _ in range(wave_length):
+                enemy = Enemy(random.randrange(50, width-100), random.randrange(-1500, -100), random.choice(['red', 'blue', 'green']))
+                enemies.append(enemy)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x - player_vel > 0: # left
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < width: # right
+            player.x += player_vel
+        if keys[pygame.K_w] and player.y - player_vel > 0: # up
+            player.y -= player_vel
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() + 15 < height: # down
+            player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot()
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            enemy.move_lasers(laser_vel, player)
+
+            if random.randrange(0, 2*60) == 1:
+                enemy.shoot()
+
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+            elif enemy.y + enemy.get_height() > height:
+                lives -= 1
+                enemies.remove(enemy)
+
+        player.move_lasers(-laser_vel, enemies)
+
+
+
+def main_menu():
+    title_font = pygame.font.SysFont('comicsans', 70)
+    run = True
+    while run:
+        window.blit(background, (0,0))
+        title_label = title_font.render('Press the mouse to begin...', 1, (255,255,255))
+        subtitle_label = title_font.render('Use WASD keys to move', 1, (255, 255, 255))
+        window.blit(title_label, (width/2 - title_label.get_width()/2, 350))
+        window.blit(subtitle_label, (width/2 - subtitle_label.get_width()/2, 500))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()
+
+main_menu()
+
 
